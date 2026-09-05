e06ff5ff3353d5668a8c8db6be2cf9d0eb9f5a13
Pablo Robles <pablorom9@gmail.com>
2026-09-05 16:07:15 +0200

    chore: bootstrap Next.js application

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/app/globals.css",
+};
+
+export default config;
